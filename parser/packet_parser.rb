fcb83f0c68fa2c71f766967fc58a22a4ab834077
Mr Rogers <jon@carbonfive.com>
2018-07-20 15:48:12 -0700

    stupid merge typo fix

--- parser/packet_parser.rb
+++ parser/packet_parser.rb
@@ -140,7 +140,6 @@ class DataDog
 
   # this send_event() is specific to DataDog. If we use a difference cloud, we'd build a new class and change/customize send_event()
   def send_event(packet)
-<<<<<<< HEAD
     time = Time.parse(packet.timestamp)
 
     # DataDog can only receive data in this format: key : value, where value is (time, metric)
