81a2f96de1526ce391fe15073cacad5495d6e1ec
Mr Rogers <jon@carbonfive.com>
2018-07-25 17:27:24 -0700

    Refactor bash script into functions that can be reused
    * move functions about ble sniffing into a bash lib that can be reused
    * update the current `tag_scanner.sh` to use the newly refactored functions

--- sniffer/tag_scanner.sh
+++ sniffer/tag_scanner.sh
@@ -17,7 +17,7 @@
 #    & = run this command in the background since we need to run hcitool and hcidump simultaneously
 #
 # 2. Verify that the scanner started using the command: `if [ "$(pidof hcitool)" ]; then`
-#    pidof = finds the process ID of a running program.  https://linux.die.net/man/8/pidof
+#    pidof = finds the process ID of a running program.  https://linux.die1.net/man/8/pidof
 #    if ["... = Evaluate whether there's a process ID. If there's no ID, the program hasn't started, therefore error.
 #
 # 3. Output the data from the BLE scanner using the command: `sudo hcidump -t --raw | read_blescan_packet_dump`
@@ -40,78 +40,25 @@
 # The hcitool will run indfinitely, so this function specifies that we kill it
 # https://www.quora.com/What-is-the-difference-between-the-SIGINT-and-SIGTERM-signals-in-Linux
 # https://www.computerhope.com/unix/utrap.htm
-halt_hcitool_lescan() {
-  sudo pkill --signal SIGINT hcitool
-}
+
+# include our bash hci helper functions
+script_root_dir="$(dirname "$0")"
+source "${script_root_dir}/../lib/hci_functions.bash"
+
 trap halt_hcitool_lescan INT
 
-# process_complete_packet is our function that removes carrots and whitespace from a data packet
-process_complete_packet() {
-  # take the first argument, $1, and set it to be the packet variable
-  # also, in the packet: find any character that is "\ " (escape-character white space) or ">" (a greater than character).
-  # Replace those characters with nothing -- because there's nothing in the first two slashes "//"
-  local packet=${1//[\ |>]/}
-  # take the second argument, $2, and  set it to be the timestamp variable
-  local timestamp=${2}
-
-  # We're looking for Fujitsu packets which we know have payloads containing 010003000300
-  # If the BLE packet doesn't containt 010003000300 then skip the output of this packet
-  if [[ ! $packet =~ 010003000300 ]]; then
-    return
-  fi
-
-  # If we reached this spot in the script, then we're dealing with a Fujitsu packet
-  # output as JSON for easy consumption
-  echo "{ \"timestamp\": \"$timestamp\", \"packet_data\": \"$packet\" }"
-}
-
-# This function reads and assembles the packet because packets span multiple lines and need to be built up
-# This function is unclear -- Mike and Jon to discuss
-
-WITH_TIMESTAMP_REGEX="^([0-9]{4}-[0-9]{2}-[0-9]{2}.*)\s+>(.*)$"
-WITHOUT_TIMESTAMP_REGEX="^()>(.*)$"
-
-read_blescan_packet_dump() {
-
-  # start with an empty string(?)
-  packet=""
-  # read a line and look for the starting character ">" or with timestamp like "2018-07-18 10:56:08.151507 >"
-  while read line; do
-    # packets start with ">" ### Mike got lost here. Are we actually looking for the beginning of the *next* packet??
-    if [[ $line =~ $WITH_TIMESTAMP_REGEX ]] || [[ $line =~ $WITHOUT_TIMESTAMP_REGEX  ]]; then
-      # extract the regex matches immediately
-      tmp_timestamp=${BASH_REMATCH[1]}
-      tmp_packet=${BASH_REMATCH[2]}
-
-      # process the completed packet (unless this is the first time through)  ### Is this unique to the first run or any new packet?
-      if [ "$packet" ]; then
-        process_complete_packet "$packet" "$timestamp"
-      fi
-
-      # start the new packet
-      timestamp=$tmp_timestamp
-      packet=$tmp_packet
-    else
-      # continue building the packet
-      packet="$packet $line"
-    fi
-  done
-}
 
 # Here's where the functions stop and the actual scanning begins (?)
 # begin BLE scanning
-# run output to the trash (/dev/null -- throw away the data stream)
-# & run this in the background
-sudo hcitool lescan > /dev/null &
-# sleep to pause for 2 seconds so that hcitool can launch
-sleep 2
+start_hcitool_lescan
+
 # make sure the scan started by finding the process ID of a running program using pidof. If there's no ID, the program hasn't started yet.
 # pidof is defined here: https://linux.die.net/man/8/pidof
 # It looks like "$()" creates an array, so the if statement is checking whether the array is null (???)
 # $() is defined here https://stackoverflow.com/questions/5163144/what-are-the-special-dollar-sign-shell-variables
 if [ "$(pidof hcitool)" ]; then
   # start the scan packet dump (with timestamps -t) and process the stream of payloads to be formatted for easier processing.
-  sudo hcidump -t --raw | read_blescan_packet_dump
+  start_hcidump_stream | read_blescan_packet_dump | process_and_filter_fujitsu_packets
 else
   # echo standard out and standard error. >&2 redirect to output on error.
   # if we said each "blah blah" >&1 it'd be redundant
