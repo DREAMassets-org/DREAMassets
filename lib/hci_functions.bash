4b3c96d1cd63309af2de0f3b0a59c0adae1e065d
mikimer <michael.vladimer@gmail.com>
2018-07-26 17:08:21 -0700

    finalized comments. made function names more precise.

--- lib/hci_functions.bash
+++ lib/hci_functions.bash
@@ -1,65 +1,8 @@
 ######
+# This is a library of functions that Mike and Jon created for `tag_scanner.sh` and other bash scripts that need to read BLE data
 # This bash script includes several functions that help with scanning the BLE packets floating around in the ether.
 #
 # To use the bash script, you must have the `bluez` and `bluez-hcidump` packages installed on the RasPi 
-
-# ??? DEFINE WHAT A "PACKET IS"
-# SHOULD WE DIFFERENTIATE PACKET VS PAYLOAD? 
-
-# PROBLEM: 
-# We need to read, aggregate, filter, and JSON-ify the Bluethooth data to make it useful
-# RasPi gathers BLE data by running two commands simultaneously: `hcitool` and `hcidump`. 
-# The output from hcitool is a list of BLE device IDs and labels, but no data.
-# The output from hcidump is a timestamp followed by raw BLE data packets. 
-#
-# Here's sample output of device IDs and labels from hcitool: 
-# 1E:91:E8:B0:AF:DA (unknown)
-# 88:C6:26:CA:42:47
-# 7C:64:56:36:1F:D9 (unknown)
-# 2C:41:A1:03:20:5D LE-QC 35 V2
-# B0:34:95:38:82:5D (unknown)
-# 88:C6:26:CA:42:47 (unknown)
-# D4:A1:BD:1F:46:F2 (unknown)
-# 21:D6:07:DF:B1:04 (unknown)
-# 
-# We don't care about most of the devices in this output -- which is why we need to filter data.
-# The device ID `D4:A1:BD:1F:46:F2` is a Fujitsu beacon -- we need its data. 
-# This bash script just discards the output from hcitool  
-# We only use hcitool because hcidump won't work without hcitool. 
-# 
-# Here's sample output of timestamps and raw BLE data packets from hcidump:
-# 2018-07-26 21:15:45.220743 > 04 3E 0C 02 01 04 00 1E 4F A2 89 5C F4 00 A9
-# 2018-07-26 21:15:45.249505 > 04 3E 17 02 01 00 00 F5 73 9A 08 40 6C 0B 02 01 06 07 FF 4C
-#   00 10 02 0B 00 A0
-# 2018-07-26 21:15:45.272482 > 04 3E 21 02 01 03 01 F2 46 1F BD A1 D4 15 02 01 04 11 FF 59
-#   00 01 00 03 00 03 00 FC 01 77 00 45 00 0A 08 BC
-# 2018-07-26 21:15:45.332024 > 04 3E 2B 02 01 03 01 39 43 86 CC 64 0F 1F 1E FF 06 00 01 09
-#   20 02 58 0C 2B A8 AB D3 6C 9A 4D 5F 39 99 0C 77 56 C1 3E 9D
-#   84 E9 C1 E4 29 A2
-# 2018-07-26 21:15:45.348606 > 04 3E 17 02 01 00 01 34 4E 89 F2 B8 74 0B 02 01 06 07 FF 4C
-#   00 10 02 0B 00 A5
-# 2018-07-26 21:15:45.349272 > 04 3E 0C 02 01 04 01 34 4E 89 F2 B8 74 00 A7
-# 2018-07-26 21:15:45.382297 > 04 3E 17 02 01 00 00 48 E6 C9 32 BC AC 0B 02 01 06 07 FF 4C
-#   00 10 02 0B 00 A7
-# 
-# Again, our BLE scanner picks up tons of devices. We need to filter down this output to the Fujitsu devices we care about. 
-# In raw format, the Fujitsu device ID D4:A1:BD:1F:46:F2 becomes inverted to `F2 46 1F BD A1 D4` (shown above)
-# The bytes `00 01 00 03 00 03 00` are an identifier (010003000300) that this is a Fujitsu beacon
-# The bytes `FC 01 77 00 45 00 0A 08 BC` are useful information: temp, x-axis acceleration, y acceleration, z acceleration and RSSI.
-# Fujitsu's documentation explains how to convert the bytes to meaningful decimal values. 
-# 
-# Another problem is that the output from hcidump breaks across lines
-# 
-# SOLUTION
-# This bash script takes the output from hcidump and then 
-# (1) Aggregates data for each BLE device across line breaks. 
-# (2) Filters the output to be only data from Fujitsu beacons (i.e., containing the identifier 010003000300)
-# (3) Outputs a JSON with the timestamp and data packet: Fujitsu beacon device ID (we call them tags), measurement data, and other junk data. 
-#
-# The function `read_blescan_packet_dump` does step (1)
-# The function `process_and_filter_fujitsu_packets` does steps (2) and (3)
-# There's a separate Ruby script that uses those JSONs as input, processes the measurement data from bytes to meaningful decimal values, and sends the values to Google Cloud 
-# 
 
 
 # begin BLE scanning
@@ -81,10 +24,9 @@ start_hcidump_stream() {
   sudo hcidump -t --raw
 }
 
-# ??? To be more descriptive, could we rename this function filter_and_JSONify_fujitsu_packets 
-# This function `process_and_filter_fujitsu_packets` receives a string from `read_blescan_packet_dump`: <packet contents>|<timestamp>  
-# `process_and_filter_fujitsu_packets` returns a JSON-formatted string the Ruby script to process. 
-process_and_filter_fujitsu_packets() {
+# This function `filter_and_JSONify_fujitsu_packets` receives a string from `aggregate_data_packet`: <packet contents>|<timestamp>  
+# `filter_and_JSONify_fujitsu_packets` returns a JSON-formatted string the Ruby script to process. 
+filter_and_JSONify_fujitsu_packets() {
   while read line; do
     # validate that the line is properly formatted (<packet contents>|<timestamp>)
     if [[ $line =~ ^(.*)\s+?\|\s+?(.*)$ ]]; then
@@ -103,39 +45,26 @@ process_and_filter_fujitsu_packets() {
 }
 
 # As explained above, hcidump outputs raw BLE data packets that break across lines. 
-# We use a regex (regular expression) to identify a timestamp that comes before the data packet 
-# ??? can we simplify from WITH_TIMESTAMP_REGEX to TIMESTAMP_REGEX
-WITH_TIMESTAMP_REGEX="^([0-9]{4}-[0-9]{2}-[0-9]{2}.*)\s+>(.*)$"
+# We use a regex (regular expression) to identify a timestamp that indicates the start of a new trasnmission from a BLE device 
+TIMESTAMP_REGEX="^([0-9]{4}-[0-9]{2}-[0-9]{2}.*)\s+>(.*)$"
 
-# This function reads and assembles the packet because packets span multiple lines and need to be aggregated. 
-# For example data from hcidump looks like this: 
-# 2018-07-18 10:56:08.151507 > AA BB CC
-# DD EE FF
-# 11 22 33
 
-# so the read_blescan_packet_dump function gathers the entire data packet without spaces or line breaks: AABBCCDDEEFF112233
-# Read from an input stream that comes from `hcidump -t --raw` and return, for each packet, a string that
-# looks like
-#   <packet hex contents>|<timestamp>
-# The result should be easily parseable into the packet contents and timestamp with a regex like ^(.*)\s+?\|\s+?(.*)$
-# where the first match is the packet contents and the second is the timestamp string (2018-07-18 10:56:08.151507) 
-
-# ??? Can we rename this function to aggregate_data_packet 
-# The function `read_blescan_packet_dump` aggregates a data packet that breaks across lines
+# The function `aggregate_data_packet` aggregates a data packet that breaks across lines
 # The function outputs a complete packet and a timestamp 
-read_blescan_packet_dump() {
+aggregate_data_packet() {
   # start with an empty string(?)
   packet=""
   # read a line 
   while read line; do
-    # the important thing to remember with this code is that 
-    # see if the line contains the regex timestamp indicating that it's the beginning of a BLE trasnmission from a new device
-    # if it doesn't  
-    # # When we see this regex, we know that we've reached the end of the previous packet 
-    # and look for the starting character ">" or with timestamp like "2018-07-18 10:56:08.151507 >"
+
+    # check if the `line` contains our `TIMESTAMP_REGEX`, which indicates the start of a new trasnmission from a BLE device 
+    # we extract values from the `line` and temporarily store them in `tmp_timestamp` and `tmp_packet` 
+    # if the line is not the beginning of a new transmission, then the line is a continuation of the previous line, 
+    #  so add the `line` to the previous `packet` we already started
+    # if the line contains a new transmission, then we output the previous `packet`. 
+    #  Then start a new packet by setting `packet`=`tmp_packet` and `timestamp`=`tmp_timestamp`
   
-    # packets start with ">" ### Mike got lost here. Are we actually looking for the beginning of the *next* packet??
-    if [[ $line =~ $WITH_TIMESTAMP_REGEX ]]; then
+    if [[ $line =~ $TIMESTAMP_REGEX ]]; then
       # extract the regex matches immediately
       tmp_timestamp=${BASH_REMATCH[1]}
       tmp_packet=${BASH_REMATCH[2]}
@@ -143,6 +72,7 @@ read_blescan_packet_dump() {
       if [ "$packet" ]; then
         # remove > and whitespace from packet string
         clean_packet=${packet//[\ |>]/}
+        # output the previous packet 
         echo $clean_packet "|" $timestamp
       fi
 
--- sniffer/tag_scanner.sh
+++ sniffer/tag_scanner.sh
@@ -1,14 +1,66 @@
 #!/bin/bash
 # Declare that this is a Bash script
 # https://stackoverflow.com/questions/8967902/why-do-you-need-to-put-bin-bash-at-the-beginning-of-a-script-file
-
-# Jon pulled this Bash script for an iBeacon Scanner.  We reconfigured it for the DREAM project with Fujitsu beacons
-# The script was originally developed by Radius Networks for iBeacons. Here's the source:
-# http://stackoverflow.com/questions/21733228/can-raspberrypi-with-ble-dongle-detect-ibeacons?lq=1
-
+#
+# 
+# PROBLEM: 
+# We need to read, aggregate, filter, and JSON-ify the Bluethooth data to make it useful
+# RasPi gathers BLE data by running two commands simultaneously: `hcitool` and `hcidump`. 
+# The output from hcitool is a list of BLE device IDs and labels, but no data.
+# The output from hcidump is a timestamp followed by raw BLE data packets. 
+#
+# Here's sample output of device IDs and labels from hcitool: 
+# 1E:91:E8:B0:AF:DA (unknown)
+# 88:C6:26:CA:42:47
+# 7C:64:56:36:1F:D9 (unknown)
+# 2C:41:A1:03:20:5D LE-QC 35 V2
+# B0:34:95:38:82:5D (unknown)
+# 88:C6:26:CA:42:47 (unknown)
+# D4:A1:BD:1F:46:F2 (unknown)
+# 21:D6:07:DF:B1:04 (unknown)
+# 
+# We don't care about most of the devices in this output -- which is why we need to filter data.
+# The device ID `D4:A1:BD:1F:46:F2` is a Fujitsu beacon -- we need its data. 
+# We only use hcitool because hcidump won't work without it.  This bash script just discards the output from hcitool  
+# 
+# Here's sample output of timestamps and raw BLE data packets from hcidump:
+# 2018-07-26 21:15:45.220743 > 04 3E 0C 02 01 04 00 1E 4F A2 89 5C F4 00 A9
+# 2018-07-26 21:15:45.249505 > 04 3E 17 02 01 00 00 F5 73 9A 08 40 6C 0B 02 01 06 07 FF 4C
+#   00 10 02 0B 00 A0
+# 2018-07-26 21:15:45.272482 > 04 3E 21 02 01 03 01 F2 46 1F BD A1 D4 15 02 01 04 11 FF 59
+#   00 01 00 03 00 03 00 FC 01 77 00 45 00 0A 08 BC
+# 2018-07-26 21:15:45.332024 > 04 3E 2B 02 01 03 01 39 43 86 CC 64 0F 1F 1E FF 06 00 01 09
+#   20 02 58 0C 2B A8 AB D3 6C 9A 4D 5F 39 99 0C 77 56 C1 3E 9D
+#   84 E9 C1 E4 29 A2
+# 2018-07-26 21:15:45.348606 > 04 3E 17 02 01 00 01 34 4E 89 F2 B8 74 0B 02 01 06 07 FF 4C
+#   00 10 02 0B 00 A5
+# 2018-07-26 21:15:45.349272 > 04 3E 0C 02 01 04 01 34 4E 89 F2 B8 74 00 A7
+# 2018-07-26 21:15:45.382297 > 04 3E 17 02 01 00 00 48 E6 C9 32 BC AC 0B 02 01 06 07 FF 4C
+#   00 10 02 0B 00 A7
+# 
+# Again, our BLE scanner picks up tons of devices. We need to filter down this output to the Fujitsu devices we care about. 
+# In raw format, the Fujitsu device ID D4:A1:BD:1F:46:F2 becomes inverted to `F2 46 1F BD A1 D4` (shown above)
+# The bytes `00 01 00 03 00 03 00` are an identifier (010003000300) that this is a Fujitsu beacon
+# The bytes `FC 01 77 00 45 00 0A 08 BC` are useful information: temp, x-axis acceleration, y acceleration, z acceleration and RSSI.
+# Fujitsu's documentation explains how to convert the bytes to meaningful decimal values. 
+# 
+# Another problem is that the output from hcidump breaks across lines
+# 
+# SOLUTION
+# This bash script takes the output from hcidump and then 
+# (1) Aggregates data for each BLE device across line breaks. 
+# (2) Filters the output to be only data from Fujitsu beacons (i.e., containing the identifier 010003000300)
+# (3) Outputs a JSON with the timestamp and data packet: Fujitsu beacon device ID (we call them tags), measurement data, and other junk data. 
+#
+# The function `aggregate_data_packet` does step (1)
+# The function `filter_and_JSONify_fujitsu_packets` does steps (2) and (3)
+# There's a separate Ruby script that uses those JSONs as input, processes the measurement data from bytes to meaningful decimal values, and sends the values to Google Cloud 
+# 
+# 
 # Here's how our modified DREAM script works
-# 0. Separate from this script, there are nearby beacons using Bluetooth Low Energy (BLE) to send out data packets in advertising/broadcast mode.
-#    This Bash script sits on a Raspbeery Pi and enables the RPi to capture those data packets for later processing.
+# 0. This script expects that there are nearby beacons using Bluetooth Low Energy (BLE) to send out data packets in advertising/broadcast mode.
+#    This Bash script sits on a Raspberry Pi and expects that you've installed all the libaries it references, special for this project as well as generic. 
+#	 This script combined with a ruby script captures the BLE data packets and sends the packets to Google Cloud.
 #
 # 1. Start the BLE scanner using the command: `sudo hcitool lescan > /dev/null &`
 #    sudo = use the privelges of a superuser https://en.wikipedia.org/wiki/Sudo
@@ -18,20 +70,20 @@
 #
 # 2. Verify that the scanner started using the command: `if [ "$(pidof hcitool)" ]; then`
 #    pidof = finds the process ID of a running program.  https://linux.die1.net/man/8/pidof
-#    if ["... = Evaluate whether there's a process ID. If there's no ID, the program hasn't started, therefore error.
+#    if... = Evaluate whether there's a process ID. If there's no ID, the program hasn't started, therefore error.
 #
-# 3. Output the data from the BLE scanner using the command: `sudo hcidump -t --raw | read_blescan_packet_dump`
+# 3. Output the data from the BLE scanner using the command: `sudo hcidump -t --raw | aggregate_data_packet`
 #    sudo = superuser
 #    hcidump = get the output from the BLE scanner
 #    -t = include the timestamp when the data arrived
 #    --raw = provide the raw hexadecimal data. By default, hcidump formats and presents the data with labels. `--raw` outputs data without labels, but still needs some cleaning
-#    | read_blescan_packet_dump = pump the data into a function called read_blescan_packet_dump. "|" is for functions; if we used ">" instead, the data would go into a file with the name read_blescan_packet_dump
+#    | aggregate_data_packet = pump the data into a function called aggregate_data_packet. "|" is for functions; if we used ">" instead, the data would go into a file with the name aggregate_data_packet
 #
 #    `hcidump --raw` has two data-cleanliness problems that this Bash script solves:
 #    (1) `hcidump` returns only 40 bytes per line. If a BLE advertising packet makes the data more than 40 bytes, then hcidump line wraps.
-#    `read_blescan_packet_dump` is our function that removes the carriage returns and gathers all the data from a packet
+#    `aggregate_data_packet` is our function that removes the carriage returns and gathers all the data from a packet
 #    (2) `hcidump --raw` outputs characters that aren't bytes: ">" and " " (carrots and whitespace)
-#    `process_complete_packet` is our function that removes those characters and simplifies the data packet to just bytes.
+#    `aggregate_data_packet` also removes those characters and simplifies the data packet to just bytes.
 #
 # 4. Close with ctrl-c. We're running hcitool in the background (the `&` did this), so we need to to specify to close hcitool.
 #    Since hcidump is in the foreground, ctrl-c will just kill it.
@@ -41,27 +93,30 @@
 # https://www.quora.com/What-is-the-difference-between-the-SIGINT-and-SIGTERM-signals-in-Linux
 # https://www.computerhope.com/unix/utrap.htm
 
-# include our bash hci helper functions
+# include the library we created with bash hci helper functions  
 script_root_dir="$(dirname "$0")"
 source "${script_root_dir}/../lib/hci_functions.bash"
 
+# this line ends hcitool when we hit ctrl-c since hcitool is running in the background
 trap halt_hcitool_lescan INT
 
-
-# Here's where the functions stop and the actual scanning begins (?)
 # begin BLE scanning
 start_hcitool_lescan
 
 # make sure the scan started by finding the process ID of a running program using pidof. If there's no ID, the program hasn't started yet.
-# pidof is defined here: https://linux.die.net/man/8/pidof
-# It looks like "$()" creates an array, so the if statement is checking whether the array is null (???)
-# $() is defined here https://stackoverflow.com/questions/5163144/what-are-the-special-dollar-sign-shell-variables
 if [ "$(pidof hcitool)" ]; then
-  # start the scan packet dump (with timestamps -t) and process the stream of payloads to be formatted for easier processing.
-  start_hcidump_stream | read_blescan_packet_dump | process_and_filter_fujitsu_packets
+  # start dumping the output from BLE scanner (with timestamps -t), aggregate packets across line breaks, filter to only include Fujitsu and output in JSON format.
+  start_hcidump_stream | aggregate_data_packet | filter_and_JSONify_fujitsu_packets
 else
   # echo standard out and standard error. >&2 redirect to output on error.
   # if we said each "blah blah" >&1 it'd be redundant
   echo "ERROR: it looks like hcitool lescan isn't starting up correctly" >&2
   exit 1
 fi
+
+
+# Note 
+# Jon originally pulled this Bash script for an iBeacon Scanner.  We reconfigured it for the DREAM project with Fujitsu beacons
+# The script was originally developed by Radius Networks for iBeacons. Here's the source:
+# http://stackoverflow.com/questions/21733228/can-raspberrypi-with-ble-dongle-detect-ibeacons?lq=1
+
