88b26a5b2a6e8b266f441f30c7344f7ab3fe8a49
Mr Rogers <jon@carbonfive.com>
2018-08-02 18:08:32 -0700

    Build dream_tester.py and dream_collector.py scripts
    * dream_tester.py reads a file or stdin and uploads to Google
    * dream_collector.py runs the scanner and then uploads to Google
    
    Details:
    * move ruby code to python for reading/decoding packets and writing to google
    * move env.sh to environment.py
    * add bundle size to commandline args
    * add secrets/environment.example.py

---
+++ lib/python/dream_environment.py
@@ -0,0 +1,21 @@
+import imp
+import os
+import socket
+
+root_dir = os.path.dirname(os.path.abspath(__file__)) + "/../.."
+env = imp.load_source("environment", root_dir + "/secrets/environment.py")
+
+def fetch():
+  try:
+    return {
+      'project_id': env.SECRETS["GOOGLE_PROJECT_ID"] or os.environ["GOOGLE_PROJECT_ID"],
+      'credentials': env.SECRETS["GOOGLE_CREDENTIALS_JSON_FILE"] or os.environ["GOOGLE_CREDENTIALS_JSON_FILE"],
+      'bucket': env.SECRETS["GOOGLE_BUCKET"] or os.environ["GOOGLE_BUCKET"],
+      'directory': env.SECRETS["GOOGLE_DIRECTORY"] or os.getenv("GOOGLE_DIRECTORY"),
+      'host': socket.gethostname()
+    }
+  except TypeError:
+    print("\n***")
+    print("Did you forget to setup your environment variables?")
+    print("***\n")
+    raise
