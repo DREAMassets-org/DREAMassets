1af9bbe698e63a24b2ee51d23d6082bf4cf95cf3
Mr Rogers <jon@carbonfive.com>
2018-08-08 10:14:50 -0700

    Add error trap for UnicodeEncodeError

--- bin/dream_collector.py
+++ bin/dream_collector.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # This script was designed to run on a RaspberryPi or Cassia X1000 system that has the `bluez`
-# BLE listening software installed.  
+# BLE listening software installed.
 #
 # When you run this software, it will listen to the Bluetooth traffic, filter out the Fujitsu
 # packets, bundle those into CSVs and send them to Google Cloud Storage.
@@ -13,7 +13,6 @@ import argparse
 import sys
 import json
 import re
-
 from bluepy import btle
 
 sys.path.insert(0, 'lib/python')
@@ -30,10 +29,11 @@ env = dream_environment.fetch()
 class ScanFujitsu(btle.DefaultDelegate):
     fujitsu_packet_regex = re.compile(r'010003000300')
 
-    def __init__(self, opts, processor):
+    def __init__(self, opts, processor, logger=None):
         btle.DefaultDelegate.__init__(self)
         self.opts = opts
         self.processor = processor
+        self.logger = logger
 
     def handleDiscovery(self, packet, _unused_isNewDevice, _unused_isNewData):
 
@@ -46,27 +46,27 @@ class ScanFujitsu(btle.DefaultDelegate):
         # 043E2102010301 1C0CB35CBBD5 15 0201 04 11FF5900 0100 0300 0300 7F03 A503 C4FF A907 C3
         # 043E2102010301 F2461FBDA1D4 15 0201 04 11FF5900 0100 0300 0300 4C03 6100 BDFF 0F08 CA
         # 043E2102010301 71BF99DC8CF7 15 0201 04 11FF5900 0100 0300 0300 F904 8D00 5800 1E08 C4
-        # 
+        #
         # This is what you might see if you used `hcidump` to get the raw packet data.
         #
         # Here we're using `bluepy` which takes that data and does a bit of processing to give us
         # rssi, and tag_id (which it calls `addr`) and a slightly parsed version of the rest.
         # Check out https://ianharvey.github.io/bluepy-doc/scanentry.html for details
-        # 
-        # For the first packet listed above as a python ScanEntry() class (from bluepy) 
+        #
+        # For the first packet listed above as a python ScanEntry() class (from bluepy)
         # you might see something like the following
         #
         # > packet.addr
         # u'd5:bb:5c:b3:0c:1c
         # > packet.rssi
-        # -66 
+        # -66
         # > packet.getScanData()
         # [(1, 'Flags', '04'), (255, 'Manufacturer', '59000100030003007F03A503C4FFA907')]
-        #                                             
+        #
         # NOTE: the rssi number comes from C3 but -66 is probably not the real value.  I don't know
         # the math that bluepy does to compute that number.  When we get it from bluepy, it is
         # in dBm's
-        
+
         packet_payload = self.extract_packet_payload(packet)
         # if the packet matches a fujitsu packet
         if re.search(self.fujitsu_packet_regex, packet_payload):
@@ -76,20 +76,28 @@ class ScanFujitsu(btle.DefaultDelegate):
                 'hub_id': env['host']
             }
             # Add acceleration/temperature data which we've decoded from payload to measurement
-            measurement.update(packet_decoder.decode(packet_payload)) 
+            measurement.update(packet_decoder.decode(packet_payload))
             # add measurement to our processor
             self.processor.addMeasurement(measurement)
             if self.opts.verbose:
                 print (json.dumps(measurement))
 
     def extract_packet_payload(self, packet):
-        # extract all packet data
-        scan_data_hash = {}
-        for (sdid, desc, val) in packet.getScanData():
-            scan_data_hash[desc] = val
-        # return only the "Manufacturer" setting which is the data that 
-        # includes temp/acceleration (assuming it's a Fujitsu packet)
-        return scan_data_hash.get('Manufacturer', '')
+        try:
+            # extract all packet data
+            scan_data_hash = {}
+            for (sdid, desc, val) in packet.getScanData():
+                scan_data_hash[desc] = val
+            # return only the "Manufacturer" setting which is the data that
+            # includes temp/acceleration (assuming it's a Fujitsu packet)
+            return scan_data_hash.get('Manufacturer', '')
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            msg = "Failed to extract packet data from device %s" % packet.addr
+            self.logger and self.logger.error(msg)
+            if self.opts.verbose:
+               print(msg, file=sys.stderr)
+            return ''
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -124,7 +132,7 @@ def main():
     if arg.scan_only:
         uploader = None
     processor = FujitsuPacketProcessor(arg, uploader, logger)
-    fujitsu_listener = ScanFujitsu(arg, processor)
+    fujitsu_listener = ScanFujitsu(arg, processor, logger)
     scanner = btle.Scanner(arg.hci).withDelegate(fujitsu_listener)
 
     logger.info("Start scanning")
