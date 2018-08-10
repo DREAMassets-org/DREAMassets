5a64f44fa11128a4dedf963f6bc65dc4f1da0f28
Mr Rogers <jon@carbonfive.com>
2018-08-10 11:08:49 -0700

    Fix timeout issue

--- events_report_scheduler/main.py
+++ events_report_scheduler/main.py
@@ -9,8 +9,8 @@ class GenerateEventsReports(webapp2.RequestHandler):
         table = "measurements_table"
         url = "https://us-central1-{project}.cloudfunctions.net/generate_events_reports?bucket={bucket}&bq_dataset={dataset}&bq_table={table}".format(project=project, bucket=bucket, dataset=dataset, table=table)
         print("Fetching url {}".format(url))
-        request = urllib2.Request(url, timeout=300, headers={"cronrequest" : "true"})
-        contents = urllib2.urlopen(request).read()
+        request = urllib2.Request(url, headers={"cronrequest" : "true"})
+        contents = urllib2.urlopen(request, timeout=300).read()
 
 app = webapp2.WSGIApplication([
     ('/daily', GenerateEventsReports),
