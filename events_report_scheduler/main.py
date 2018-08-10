7684be82cc2a1bfdf3cd5f376c4cf7091c6c63bb
Mr Rogers <jon@carbonfive.com>
2018-08-09 17:28:19 -0700

    Add google app engines to schedule a daily events report generation via the generate_events_reports cloud function

---
+++ events_report_scheduler/main.py
@@ -0,0 +1,17 @@
+import webapp2
+import urllib2
+class GenerateEventsReports(webapp2.RequestHandler):
+    def get(self):
+        # Before deploying this app, make sure these are set to match your deployed cloud function
+        project = "dreamassettester"
+        bucket = "dream-assets-orange"
+        dataset = "measurements_dataset"
+        table = "measurements_table"
+        url = "https://us-central1-{project}.cloudfunctions.net/generate_events_reports?bucket={bucket}&bq_dataset={dataset}&bq_table={table}".format(project=project, bucket=bucket, dataset=dataset, table=table)
+        print("Fetching url {}".format(url))
+        request = urllib2.Request(url, headers={"cronrequest" : "true"})
+        contents = urllib2.urlopen(request).read()
+
+app = webapp2.WSGIApplication([
+    ('/daily', GenerateEventsReports),
+    ], debug=True)
