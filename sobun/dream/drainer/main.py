8e78df04e7df2273df9ecf671fdda995ff5b30e9
Rit Li <rit@carbonfive.com>
2018-10-03 17:27:16 -0700

    Use Cloud Function to insert data into BigQuery

---
+++ sobun/dream/drainer/main.py
@@ -0,0 +1,10 @@
+"""
+Cloud Function: Background Function
+
+For details, visit:
+https://cloud.google.com/functions/docs/writing/background#functions_background_parameters-python
+"""
+
+
+def run(data, context):
+    print(data)
