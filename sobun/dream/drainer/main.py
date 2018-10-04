6ab637a503eba1f042fa57bfa394092a3ecf3334
Rit Li <rit@carbonfive.com>
2018-10-03 18:58:45 -0700

    Insert data into BigQuery table

--- sobun/dream/drainer/main.py
+++ sobun/dream/drainer/main.py
@@ -5,6 +5,20 @@ For details, visit:
 https://cloud.google.com/functions/docs/writing/background#functions_background_parameters-python
 """
 
+from google.cloud import bigquery
+
+
+client = bigquery.Client()
+dataset_id = 'dream_assets_raw_packets'
+table_id = 'measurements_table'
+table_ref = client.dataset(dataset_id).table(table_id)
+table = client.get_table(table_ref)
 
 def run(data, context):
-    print(data)
+
+    rows = [
+            (data['tag_id'], data['measurements'])
+    ]
+
+    errors = client.insert_rows(table, rows)
+    assert errors == []
