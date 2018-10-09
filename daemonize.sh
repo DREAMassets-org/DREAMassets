cc9881bec9f47ba82d081024c86ee5d476b8b0fd
Rit Li <rit@carbonfive.com>
2018-10-09 09:18:52 -0700

    Assume there are four BLE sniffers on start

--- daemonize.sh
+++ daemonize.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-sudo cp ./dream-sniffer.service /etc/systemd/system/
+sudo cp ./dream-sniffer@.service /etc/systemd/system/
 sudo cp ./dream-syncer.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 
-sudo systemctl enable dream-sniffer.service
+sudo systemctl disable dream-sniffer.service
+sudo systemctl enable dream-sniffer@{0..3}.service
 sudo systemctl enable dream-syncer.service
 
-sudo systemctl restart dream-sniffer.service
+sudo systemctl restart dream-sniffer@{0..3}.service
 sudo systemctl restart dream-syncer.service
