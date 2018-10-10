dcc876ae95069f1527adfb14e738203da330d0a8
Rit Li <rit@carbonfive.com>
2018-10-10 15:50:16 -0700

    Generate 10000 dummy payloads

--- sobun/dream/genpayload.py
+++ sobun/dream/genpayload.py
@@ -1,9 +1,19 @@
 import redis
 
+from dream.syncer import push
+
 r = redis.StrictRedis()
+dummy_payload = {
+        "tag_id": "d5a0e5b5ffc1",
+        "rssi": -63,
+        "timestamp": 1539206911,
+        "mfr_data": "5900010003000300910370003f0030f8",
+        }
 
-with open('./sample_payloads.txt') as data:
-    lines = data.readlines()
-    r.rpush('celery', *lines)
 
-print("Total Payload count: {}".format(r.llen('celery')))
+if __name__ == "__main__":
+    hci = 0
+    for i in xrange(10000):
+        push.delay(dummy_payload, 0)
+
+    print("Total Payload count: {}".format(r.llen('celery')))
