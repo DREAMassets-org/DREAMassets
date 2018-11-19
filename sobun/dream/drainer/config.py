3733f3342bcd265cb1af3f9dd331a822e0b4a10e
Mike Vee <michael.vladimer@gmail.com>
2018-11-19 15:45:51 -0500

    remove config.py
    the cloud function no longer uses config.py so we removed it for clarity

--- sobun/dream/drainer/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-BIG_QUERY_DATASET_ID = os.environ.get("BIG_QUERY_DATASET_ID", "dream_assets_dataset")
-BIG_QUERY_TABLE_ID = os.environ.get("BIG_QUERY_TABLE_ID", "dream_measurements_table")
